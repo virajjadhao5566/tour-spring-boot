6c5273a9c9ba8c98c95f3fb5982fb1ed5e0262c5
Viraj Jadhav <virajjadhao85@gmail.com>
2024-02-19 11:29:28 +0530

    initial commit

---
+++ src/main/java/com/tour/project/tourSpring/TourSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tour.project.tourSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tour/project/tourSpring/TourSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tour.project.tourSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
